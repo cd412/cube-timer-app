bb95f6e06ec1ab577d663e6e3283311ea5dc7002
Chris Defreitas <christophercdefreitas@gmail.com>
2020-05-01 13:07:00 -0400

    Added basic bootstap theme

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def index():
             return "There was an issue manually adding the time."
     else:
         # Show page
-        times = Time.query.order_by(Time.date_entered).all()
+        times = Time.query.order_by(Time.date_entered.desc()).all()
         return render_template('index.html', times=times)
 
 @app.route('/delete/<int:id>')
